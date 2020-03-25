9bf08b2dff7c26cd74bf7a02e186d8870f0274f3
Eyad Kobatte <ekobatte96@gmail.com>
2020-03-25 16:55:18 +0530

    wip: Register a new user with photo

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const multer = require('multer');
 
+const logger = require('../logger');
+
+const upload = multer({ dest: 'uploads/' });
 const router = express.Router();
 
-router.post('/register', (req, res, next) => {
-  const { username, nickname } = req.body;
-  res.status(200).json({ username, nickname });
-  return next;
-});
+router.post(
+  '/register',
+  upload.fields([
+    { name: 'normalPhoto', maxCount: 1 },
+    { name: 'psychPhoto', maxCount: 1 },
+  ]),
+  (req, res, next) => {
+    const { username, nickname } = req.body;
+    logger.info({ normalPhoto: req.files.normalPhoto });
+    res.status(200).json({ username, nickname });
+    return next;
+  },
+);
 
 module.exports = router;
